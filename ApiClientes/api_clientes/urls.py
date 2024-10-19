b57abfe738bdb3bbc0cc8ff94ca6b1075ee1770b
matheus <matheusvictor870@gmail.com>
2024-10-18 22:49:41 -0300

    Basic django project layout

--- ApiClientes/api_clientes/urls.py
+++ ApiClientes/api_clientes/urls.py
@@ -1,1 +1,4 @@
-urlpatterns = []
+from api_clientes.views import Clientes
+from django.urls import path
+
+urlpatterns = [path("users/", Clientes.as_view())]
--- ApiClientes/api_clientes/views.py
+++ ApiClientes/api_clientes/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from api_clientes.serializers import ClientSerializer
+
+
+class Clientes(APIView):
+    serializer_class = ClientSerializer
+
+    def get(self, request) -> Response:
+        return Response(status=status.HTTP_200_OK)
